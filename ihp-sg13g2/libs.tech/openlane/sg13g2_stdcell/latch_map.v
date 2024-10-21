8ec5a0d94a613764fdc86d330a749e679f9df7ee
htfab <mpw@htamas.net>
2024-10-21 11:12:25 +0200

    libs.tech: Add techmap entry for $_DLATCH_N_
    Signed-off-by: htfab <mpw@htamas.net>

--- ihp-sg13g2/libs.tech/openlane/sg13g2_stdcell/latch_map.v
+++ ihp-sg13g2/libs.tech/openlane/sg13g2_stdcell/latch_map.v
@@ -15,6 +15,15 @@ module \$_DLATCH_PN0_ (input E, input R, input D, output Q);
   );
 endmodule
 
+module \$_DLATCH_N_ (input E, input D, output Q);
+  sg13g2_dllrq_1 _TECHMAP_DLATCH_N (
+    .D(D),
+    .Q(Q),
+    .GATE_N(E),
+    .RESET_B(1'b1)
+  );
+endmodule
+
 module \$_DLATCH_NN0_ (input E, input R, input D, output Q);
   sg13g2_dllrq_1 _TECHMAP_DLATCH_NN0 (
     .D(D),
